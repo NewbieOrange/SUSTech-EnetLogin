e93bd2852b4e1b3b4d872ddbe84723bfb5dd9cf0
NewbieOrange <yutiancheng1999@gmail.com>
2020-08-20 19:43:17 +0800

    Minor cleanup and reformat

--- enetlogin.sh
+++ enetlogin.sh
@@ -1,23 +1,26 @@
+#!/bin/bash
 loginurl="https://cas.sustech.edu.cn/cas/login"
 authip="219.134.142.194"
+useragent="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
 # Insert your CAS info below:
-username="YOUR_USER_NAME_HERE"
+username="YOUR_USERNAME_HERE"
 password="YOUR_PASSWORD_HERE"
+interface="eth0"
+curl_interface="eth0"
 
-while [ true ]
-do
-	ret_code=`curl -I -s --connect-timeout 5 http://www.baidu.com -w %{http_code} | tail -n1`
+while [ true ] ; do
+	ret_code=`curl --interface "$interface" -I -s --connect-timeout 5 http://www.baidu.com -w %{http_code} | tail -n1`
 
 	if [ $ret_code -ne 200 ] ; then
 		echo "Attempting to log in the enet system"
 		rm -f /tmp/cascookie
 
-		# You may need to modify the following regex for different distros.
-		routerip=$(ifconfig | grep -A 1 "^eth0.2" | grep -P -o "(?<=inet addr:).*(?=  Bcast)")
+		# You may need to modify the following regex for different distros / versions of OpenWrt.
+		routerip=$(ifconfig | grep -A 1 "^$interface" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
 		eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=$routerip&wlanacip=$authip"
-		execution=$(curl --silent --cookie-jar /tmp/cascookies -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "$eneturl"  | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
+		execution=$(curl --interface "$curl_interface" --silent --cookie-jar /tmp/cascookies -H "$useragent" -L "$eneturl"  | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
 		
-		curl --silent --output /dev/null --cookie /tmp/cascookies --cookie-jar /tmp/cascookies -H "Content-Type: application/x-www-form-urlencoded" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0" -L -X POST "$loginurl" --data "username=$username&password=$password&execution=$execution&_eventId=submit&geolocation="
+		curl --interface "$curl_interface" --silent --output /dev/null --cookie /tmp/cascookies --cookie-jar /tmp/cascookies -H "Content-Type: application/x-www-form-urlencoded" -H "$useragent" -L -X POST "$loginurl" --data "username=$username&password=$password&execution=$execution&_eventId=submit&geolocation="
 	else
 		echo "Connected to Internet, recheck a second later"
 	fi
