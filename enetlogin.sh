e6329f792f865131be37b5dadad4f27f5b2c5390
Killer_Quinn <51754303+Certseeds@users.noreply.github.com>
2020-08-22 20:10:49 +0800

    make it more easy to read.

--- enetlogin.sh
+++ enetlogin.sh
@@ -1,23 +1,48 @@
-loginurl="https://cas.sustech.edu.cn/cas/login"
+#!/bin/bash
+#set -eoux pipefail
+oginurl="https://cas.sustech.edu.cn/cas/login"
 authip="219.134.142.194"
+USER_AGENT="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0"
 # Insert your CAS info below:
 username="YOUR_USER_NAME_HERE"
 password="YOUR_PASSWORD_HERE"
 
-while [ true ]
-do
-	ret_code=`curl -I -s --connect-timeout 5 http://www.baidu.com -w %{http_code} | tail -n1`
+while [ true ] ; do
+	ret_code=$(curl \
+	    -I -s --connect-timeout 5 \
+	    http://www.baidu.com -w %{http_code} \
+	    | tail -n1)
 
-	if [ $ret_code -ne 200 ] ; then
+	if [ ${ret_code} -ne 200 ] ; then
 		echo "Attempting to log in the enet system"
 		rm -f /tmp/cascookie
 
 		# You may need to modify the following regex for different distros.
-		routerip=$(ifconfig | grep -A 1 "^eth0.2" | grep -P -o "(?<=inet addr:).*(?=  Bcast)")
-		eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=$routerip&wlanacip=$authip"
-		execution=$(curl --silent --cookie-jar /tmp/cascookies -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0" -L "$eneturl"  | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
+		routerip=$(ifconfig \
+		    | grep -A 1 "^eth0.2" \
+			| grep -P -o "(?<=inet addr:).*(?=  Bcast)")
 		
-		curl --silent --output /dev/null --cookie /tmp/cascookies --cookie-jar /tmp/cascookies -H "Content-Type: application/x-www-form-urlencoded" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0" -L -X POST "$loginurl" --data "username=$username&password=$password&execution=$execution&_eventId=submit&geolocation="
+		eneturl=$(printf \
+		    "http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=%s&wlanacip=%s" \
+		    ${routerip} ${authip})
+
+		execution=$(curl --silent \
+		    --cookie-jar /tmp/cascookies \
+			-H "User-Agent: "${USER_AGENT}"" \
+			-L "${eneturl}" \
+			| grep -o 'execution.*/><input type' \
+			| grep -o '[^"]\{50,\}')
+		
+		curl --silent \
+		    --output /dev/null \
+			--cookie /tmp/cascookies \
+			--cookie-jar /tmp/cascookies \
+			-H "Content-Type: application/x-www-form-urlencoded" \
+			-H "User-Agent: "${USER_AGENT}"" \
+			-L -X POST "${loginurl}" \
+			--data $(printf \
+			    "username=%s&password=%s&execution=%s&_eventId=submit&geolocation=" \
+				${username} ${password} ${execution})
 	else
 		echo "Connected to Internet, recheck a second later"
 	fi
--- entlogin_asus_merlin.sh
+++ entlogin_asus_merlin.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 ###
  # @Author: your name
  # @Date: 2020-08-21 19:26:14
- # @LastEditTime: 2020-08-21 21:13:28
+ # @LastEditTime: 2020-08-22 20:07:55
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: \SUSTech-EnetLogin\temp.sh
 ### 
-set -euxo pipefail
+set -e pipefail
 loginurl="https://cas.sustech.edu.cn/cas/login"
 authip="219.134.142.194"
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.112 Safari/537.36"
 # Insert your CAS info below:
 username="YOUR_USER_NAME_HERE"
 password="YOUR_PASSWORD_HERE"
 
-USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.112 Safari/537.36"
-while [[ true ]] ; do
-  ret_code=$(
-    curl \
+while [ true ] ; do
+  ret_code=$(curl \
       -I -s --connect-timeout 5 \
 	  http://www.baidu.com -w %{http_code} \
 	  | tail -n1)
     #　it should be a number
-  if [[ ${ret_code} -ne 200 ]] ; then
+  if [ ${ret_code} -ne 200 ] ; then
 	echo "Attempting to log in the enet system"
 	rm -f /tmp/cascookie
 
@@ -39,7 +38,7 @@ while [[ true ]] ; do
 	  --connect-timeout 5 \
 	  --cookie-jar /tmp/cascookies\
 	  -H "User-Agent: "${USER_AGENT}""\
-	  -L "$eneturl" \
+	  -L "${eneturl}" \
 	  | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
 
 	curl --silent \
@@ -50,7 +49,9 @@ while [[ true ]] ; do
       -H "Content-Type: application/x-www-form-urlencoded" \
       -H "User-Agent: "${USER_AGENT}"" \
       -L -X POST "${loginurl}" \
-	  --data "username=${username}&password=${password}&execution=${execution}&_eventId=submit&geolocation="
+	  --data $(printf \
+		"username=%s&password=%s&execution=%s&_eventId=submit&geolocation=" \
+		${username} ${password} ${execution})
 
   else
     echo "Connected to Internet, recheck a second later"
