167f32e4b1320e44ddfeec293f3159cdb6c74654
NewbieOrange <NewbieOrange@users.noreply.github.com>
2020-08-25 16:58:54 +0800

    Make intent 4 spaces and minor enhancements

--- enetlogin.sh
+++ enetlogin.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#set -eoux pipefail
-useragent="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
+useragent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"
 loginurl="https://cas.sustech.edu.cn/cas/login"
 authip="219.134.142.194"
 # Insert your CAS info below:
@@ -8,30 +7,28 @@ username="YOUR_USERNAME_HERE"
 password="YOUR_PASSWORD_HERE"
 interface="eth0"
 
-while [ true ] ; do
-	ret_code=`curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1`
+while [ true ]; do
+	ret_code=$(curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1)
 
-	if [ ${ret_code} -ne 200 ] ; then
+	if [ ${ret_code} -ne 200 ]; then
 		echo "Attempting to log in the enet system"
 		rm -f /tmp/cascookie
 
 		# You may need to modify the following regex for different distros / versions of OpenWrt.
 		routerip=$(ifconfig | grep -A 1 "^${interface}" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
-		
-		eneturl=$(printf "http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=%s&wlanacip=%s" ${routerip} ${authip})
+
+		eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=${routerip}&wlanacip=${authip}"
 
 		execution=$(curl --silent --cookie-jar /tmp/cascookies \
-			-H "User-Agent: ${useragent}" -L "${eneturl}" \
-			| grep -o 'execution.*/><input type' \
-			| grep -o '[^"]\{50,\}')
-		
+			-H "User-Agent: ${useragent}" -L "${eneturl}" |
+			grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
+
 		curl --silent --output /dev/null \
 			--cookie /tmp/cascookies --cookie-jar /tmp/cascookies \
 			-H "Content-Type: application/x-www-form-urlencoded" \
 			-H "User-Agent: ${useragent}" \
 			-L -X POST "${loginurl}" \
-			--data "$(printf "username=%s&password=%s&execution=%s&_eventId=submit&geolocation=" \
-				${username} ${password} ${execution})"
+            --data "username=${username}&password=${password}&execution=${execution}&_eventId=submit&geolocation="
 	else
 		echo "Connected to Internet, recheck a second later"
 	fi
--- entlogin_asus_merlin.sh
+++ entlogin_asus_merlin.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e pipefail
 useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.112 Safari/537.36"
 loginurl="https://cas.sustech.edu.cn/cas/login"
 authip="219.134.142.194"
@@ -8,34 +7,31 @@ username="YOUR_USERNAME_HERE"
 password="YOUR_PASSWORD_HERE"
 interface="eth0"
 
-while [ true ] ; do
-  ret_code=$(curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1)
+while [ true ]; do
+    ret_code=$(curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1)
 
-  if [ ${ret_code} -ne 200 ] ; then
-	echo "Attempting to log in the enet system"
-	rm -f /tmp/cascookie
+    if [ ${ret_code} -ne 200 ]; then
+        echo "Attempting to log in the enet system"
+        rm -f /tmp/cascookie
 
-	# You may need to modify the following regex for different versions of firmware.
-	routerip=$(ifconfig |  grep -A 1 "^${interface}" \
-	  | awk '{gsub(/^\s+|\s+$/, "");print}' \
-	  | sed -n "2,2p" |  awk -F "[: ]" '{print $3}')
+        # You may need to modify the following regex for different versions of firmware.
+        routerip=$(ifconfig | grep -A 1 "^${interface}" | awk '{gsub(/^\s+|\s+$/, "");print}' |
+            sed -n "2,2p" | awk -F "[: ]" '{print $3}')
 
-	eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=${routerip}&wlanacip=${authip}"
+        eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=${routerip}&wlanacip=${authip}"
 
-	execution=$(curl --silent --cookie-jar /tmp/cascookies \
-	  -H "User-Agent: ${useragent}" -L "${eneturl}" \
-	  | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
-    
-	#The above and below commands will execute for a long time
+        execution=$(curl --silent --cookie-jar /tmp/cascookies \
+            -H "User-Agent: ${useragent}" -L "${eneturl}" |
+            grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
 
-	curl --silent -output /dev/null \
-	  --cookie /tmp/cascookies --cookie-jar /tmp/cascookies \
-	  -H "Content-Type: application/x-www-form-urlencoded" \
-	  -H "User-Agent: ${useragent}" -L -X POST "${loginurl}" \
-	  --data "$(printf "username=%s&password=%s&execution=%s&_eventId=submit&geolocation=" \
-	    ${username} ${password} ${execution})"
-  else
-    echo "Connected to Internet, recheck a second later"
-  fi
+        curl --silent --output /dev/null \
+            --cookie /tmp/cascookies --cookie-jar /tmp/cascookies \
+            -H "Content-Type: application/x-www-form-urlencoded" \
+            -H "User-Agent: ${useragent}" \
+            -L -X POST "${loginurl}" \
+            --data "username=${username}&password=${password}&execution=${execution}&_eventId=submit&geolocation="
+    else
+        echo "Connected to Internet, recheck a second later"
+    fi
     sleep 1s
 done
