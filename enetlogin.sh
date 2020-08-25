4b28f09623c80f3ca1c9e91f23c807c26aff00a4
NewbieOrange <NewbieOrange@users.noreply.github.com>
2020-08-25 17:02:04 +0800

    Replace tabs with 4 spaces

--- enetlogin.sh
+++ enetlogin.sh
@@ -8,29 +8,29 @@ password="YOUR_PASSWORD_HERE"
 interface="eth0"
 
 while [ true ]; do
-	ret_code=$(curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1)
+    ret_code=$(curl --interface "${interface}" -I -s --connect-timeout 3 http://www.baidu.com -w %{http_code} | tail -n1)
 
-	if [ ${ret_code} -ne 200 ]; then
-		echo "Attempting to log in the enet system"
-		rm -f /tmp/cascookie
+    if [ ${ret_code} -ne 200 ]; then
+        echo "Attempting to log in the enet system"
+        rm -f /tmp/cascookie
 
-		# You may need to modify the following regex for different distros / versions of OpenWrt.
-		routerip=$(ifconfig | grep -A 1 "^${interface}" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
+        # You may need to modify the following regex for different distros / versions of OpenWrt.
+        routerip=$(ifconfig | grep -A 1 "^${interface}" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
 
-		eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=${routerip}&wlanacip=${authip}"
+        eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=${routerip}&wlanacip=${authip}"
 
-		execution=$(curl --silent --cookie-jar /tmp/cascookies \
-			-H "User-Agent: ${useragent}" -L "${eneturl}" |
-			grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
+        execution=$(curl --silent --cookie-jar /tmp/cascookies \
+            -H "User-Agent: ${useragent}" -L "${eneturl}" |
+            grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
 
-		curl --silent --output /dev/null \
-			--cookie /tmp/cascookies --cookie-jar /tmp/cascookies \
-			-H "Content-Type: application/x-www-form-urlencoded" \
-			-H "User-Agent: ${useragent}" \
-			-L -X POST "${loginurl}" \
+        curl --silent --output /dev/null \
+            --cookie /tmp/cascookies --cookie-jar /tmp/cascookies \
+            -H "Content-Type: application/x-www-form-urlencoded" \
+            -H "User-Agent: ${useragent}" \
+            -L -X POST "${loginurl}" \
             --data "username=${username}&password=${password}&execution=${execution}&_eventId=submit&geolocation="
-	else
-		echo "Connected to Internet, recheck a second later"
-	fi
-	sleep 1s
+    else
+        echo "Connected to Internet, recheck a second later"
+    fi
+    sleep 1s
 done
